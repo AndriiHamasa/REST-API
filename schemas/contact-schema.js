be2918dcab93a111d4ea58c542a175c314ffd3c1
AndriiHamasa <andrii.khamaza@nure.ua>
2023-08-02 09:41:48 +0300

    create file .env.example and fixed some errors

--- schemas/contact-schema.js
+++ schemas/contact-schema.js
@@ -2,8 +2,8 @@ import Joi from "joi";
 
 export const contactAddSchema = Joi.object({
   name: Joi.string().required().messages({'any.required': `missing required name field`}),
-  email: Joi.string().messages({'any.required': `missing required email field`}),
-  phone: Joi.string().messages({ 'any.required': `missing required phone field` }),
+  email: Joi.string().required().messages({'any.required': `missing required email field`}),
+  phone: Joi.string().required().messages({ 'any.required': `missing required phone field` }),
   favorite: Joi.boolean() 
 });
 
